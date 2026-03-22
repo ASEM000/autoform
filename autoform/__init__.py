196c381a340ff4f46ce44cfad2e01cc1d6cacf63
Mahmoud Asem <48389287+ASEM000@users.noreply.github.com>
2026-03-22 20:20:34 +0900

    feat/inference (#32)
    * feat: add inference
    
    * docs: add to api
    
    * docs: add  cl entry

--- autoform/__init__.py
+++ autoform/__init__.py
@@ -20,6 +20,7 @@ from autoform.checkpoint import checkpoint, collect, inject
 from autoform.control import stop_gradient, switch, while_loop
 from autoform.core import acall, call, trace
 from autoform.dce import dce
+from autoform.inference import factor, weight
 from autoform.lm import lm_call, struct_lm_call, using_router
 from autoform.memoize import memoize
 from autoform.scheduling import depends, gather, sched
@@ -43,6 +44,7 @@ __all__ = [
     "dce",
     "sched",
     "memoize",
+    "weight",
     # primitives
     "format",
     "concat",
@@ -57,6 +59,7 @@ __all__ = [
     "while_loop",
     "gather",
     "depends",
+    "factor",
     # types
     "Struct",
     "PYTREE_NAMESPACE",
---
+++ tests/test_inference.py
@@ -0,0 +1,172 @@
+# Copyright 2026 The autoform Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import pytest
+
+import autoform as af
+
+
+def len_score(text: str) -> float:
+    return float(len(text))
+
+
+class TestFactor:
+    def test_factor_returns_local_score(self):
+        def program(x):
+            return af.factor(x, judge=len_score)
+
+        ir = af.trace(program)("x")
+        assert af.call(ir)("hello") == 5.0
+
+    def test_factor_batches_elementwise(self):
+        def program(x):
+            return af.factor(x, judge=len_score)
+
+        ir = af.trace(program)("x")
+        batched_ir = af.batch(ir)
+        assert af.call(batched_ir)(["a", "bbb"]) == [1.0, 3.0]
+
+    def test_factor_pushforward_is_unsupported(self):
+        def program(x):
+            return af.factor(x, judge=len_score)
+
+        ir = af.trace(program)("x")
+        pf_ir = af.pushforward(ir)
+
+        with pytest.raises(NotImplementedError, match="no default pushforward rule"):
+            af.call(pf_ir)(("hello", "delta"))
+
+    def test_factor_pullback_is_unsupported(self):
+        def program(x):
+            return af.factor(x, judge=len_score)
+
+        ir = af.trace(program)("x")
+        pb_ir = af.pullback(ir)
+
+        with pytest.raises(NotImplementedError, match="no default pullback rule"):
+            af.call(pb_ir)(("hello", 1.0))
+
+
+class TestWeight:
+    def test_weight_sums_executed_factors(self):
+        def program(x):
+            y = af.concat(x, "!")
+            af.factor(x, judge=len_score)
+            af.factor(y, judge=len_score)
+            return y
+
+        ir = af.trace(program)("x")
+        weight_ir = af.weight(ir)
+
+        output, total = af.call(weight_ir)("ab")
+        assert output == "ab!"
+        assert total == 5.0
+
+    def test_weight_only_counts_taken_switch_branch(self):
+        def left(x):
+            out = af.concat("L:", x)
+            af.factor(out, judge=len_score)
+            return out
+
+        def right(x):
+            out = af.concat("R:", x)
+            af.factor(x, judge=len_score)
+            return out
+
+        branches = {
+            "left": af.trace(left)("x"),
+            "right": af.trace(right)("x"),
+        }
+
+        def program(key, x):
+            return af.switch(key, branches, x)
+
+        ir = af.trace(program)("left", "x")
+        weight_ir = af.weight(ir)
+
+        left_out, left_total = af.call(weight_ir)("left", "go")
+        right_out, right_total = af.call(weight_ir)("right", "go")
+
+        assert left_out == "L:go"
+        assert left_total == 4.0
+        assert right_out == "R:go"
+        assert right_total == 2.0
+
+    def test_weight_counts_loop_iterations(self):
+        def cond(x):
+            return af.match(x, "")
+
+        def body(x):
+            new_x = af.concat(x, "x")
+            af.factor(new_x, judge=len_score)
+            return new_x
+
+        cond_ir = af.trace(cond)("")
+        body_ir = af.trace(body)("")
+
+        def loop(init):
+            return af.while_loop(cond_ir, body_ir, init, max_iters=3)
+
+        ir = af.trace(loop)("")
+        weight_ir = af.weight(ir)
+
+        out1, total1 = af.call(weight_ir)("")
+        out2, total2 = af.call(weight_ir)("seed")
+
+        assert out1 == "x"
+        assert total1 == 1.0
+        assert out2 == "seed"
+        assert total2 == 0.0
+
+    def test_batch_of_weight_returns_per_trajectory_totals(self):
+        def program(x):
+            y = af.concat(x, "!")
+            af.factor(y, judge=len_score)
+            return y
+
+        ir = af.trace(program)("x")
+        weight_ir = af.weight(ir)
+        batched_ir = af.batch(weight_ir)
+
+        outputs, totals = af.call(batched_ir)(["a", "bbb"])
+        assert outputs == ["a!", "bbb!"]
+        assert totals == [2.0, 4.0]
+
+    def test_dce_keeps_factor_equations(self):
+        def program(x):
+            y = af.concat(x, "!")
+            af.factor(y, judge=len_score)
+            return x
+
+        ir = af.trace(program)("x")
+        dced = af.dce(ir)
+        weight_ir = af.weight(dced)
+
+        assert len(dced.ir_eqns) == 2
+        output, total = af.call(weight_ir)("ab")
+        assert output == "ab"
+        assert total == 3.0
+
+    @pytest.mark.asyncio(loop_scope="function")
+    async def test_weight_async(self):
+        def program(x):
+            af.factor(x, judge=len_score)
+            return af.concat(x, "!")
+
+        ir = af.trace(program)("x")
+        weight_ir = af.weight(ir)
+
+        output, total = await af.acall(weight_ir)("abc")
+        assert output == "abc!"
+        assert total == 3.0
