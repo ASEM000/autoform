037842cfe36e27452d5a0b2b16d168790221ae9d
Mahmoud Asem <mahmoudasem00@gmail.com>
2026-08-12 16:51:44 +0900

    tests: add ad spaces tests

--- tests/test_ir.py
+++ tests/test_ir.py
@@ -94,6 +94,48 @@ class TestSpace:
         with pytest.raises(AssertionError, match="Expected bool for replace"):
             space.set(Blob, lambda value: BlobAVal(value.size), replace=1)
 
+    @pytest.mark.parametrize(
+        ("space", "aval"),
+        [
+            (space, aval)
+            for space in (af.core.tangent_s, af.core.cotangent_s)
+            for aval in (
+                af.core.StrAVal(),
+                af.core.IntAVal(),
+                af.core.FloatAVal(),
+                af.core.BoolAVal(),
+            )
+        ],
+    )
+    def test_builtin_ad_spaces_preserve_aval(self, space, aval):
+        assert space.avalof(aval) is aval
+
+    def test_custom_ad_spaces(self):
+        class TextAVal(af.core.AVal):
+            pass
+
+        class TextEditAVal(af.core.AVal):
+            pass
+
+        class TextFeedbackAVal(af.core.AVal):
+            pass
+
+        tangent_s = af.core.Space("tangent")
+        cotangent_s = af.core.Space("cotangent")
+        tangent_s.set(TextAVal, lambda _: TextEditAVal())
+        cotangent_s.set(TextAVal, lambda _: TextFeedbackAVal())
+
+        assert isinstance(tangent_s.avalof(TextAVal()), TextEditAVal)
+        assert isinstance(cotangent_s.avalof(TextAVal()), TextFeedbackAVal)
+
+    @pytest.mark.parametrize("space", [af.core.tangent_s, af.core.cotangent_s])
+    def test_missing_ad_space_rule(self, space):
+        class UnknownAVal(af.core.AVal):
+            pass
+
+        with pytest.raises(TypeError, match=f"No {space.name} aval rule registered"):
+            space.avalof(UnknownAVal())
+
 
 class TestBuildIR:
     def test_trace_scalar_input_is_dynamic(self):
